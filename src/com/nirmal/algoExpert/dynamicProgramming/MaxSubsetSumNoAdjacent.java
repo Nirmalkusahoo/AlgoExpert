cdaf2fb83df648f43af8692f25be8ed3bfb9515e
Nirmal Kumar Sahoo <nirmal.kumar.sahoo@sap.com>
2021-11-28 09:36:21 +0530

    BinarySearch

--- src/com/nirmal/algoExpert/dynamicProgramming/MaxSubsetSumNoAdjacent.java
+++ src/com/nirmal/algoExpert/dynamicProgramming/MaxSubsetSumNoAdjacent.java
@@ -1,27 +1,44 @@
 package com.nirmal.algoExpert.dynamicProgramming;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MaxSubsetSumNoAdjacent {
     public static int maxSubsetSumNoAdjacent(int[] array) {
         // Write your code here.
-        int largest= -999999;
-        for(int i=2;i<array.length-2;i++){
-            int leftIndex=i-2;
-            int rightIndex=i+2;
-            while(leftIndex>=0 && rightIndex<=array.length-2){
-                int sum =array[i]+array[leftIndex]+array[rightIndex];
-                if(sum>largest){
-                    largest=sum;
-                }
-                leftIndex--;
-                rightIndex++;
-            }
-
+        if (array.length == 0) {
+            return 0;
+        } else if (array.length == 1) {
+            return array[0];
         }
-        return largest;
+
+        int second = array[0];
+        int first = Math.max(array[0], array[1]);
+
+        for (int i = 2; i < array.length; i++) {
+            int current = Math.max(first, second + array[i]);
+            second = first;
+            first = current;
+        }
+        return first;
     }
+
+    // Solution 1:
+    /*public static int maxSubsetSumNoAdjacent(int[] array) {
+        // Write your code here.
+        if(array.length ==0){
+            return 0;
+        }
+        else if(array.length ==1){
+            return array[0];
+        }
+
+        int[] maxSums=array.clone();
+        maxSums[1]= Math.max(array[1],array[0]);
+
+        for(int i=2;i<array.length;i++){
+            maxSums[i]= Math.max(maxSums[i-1],maxSums[i-2]+ array[i]);
+        }
+
+        return maxSums[array.length-1];
+    }*/
 
     public static void main(String[] args) {
         System.out.println(MaxSubsetSumNoAdjacent.maxSubsetSumNoAdjacent(new int[]{75, 105, 120, 75, 90, 135}));
---
+++ src/com/nirmal/algoExpert/search/BinarySearch.java
@@ -0,0 +1,49 @@
+package com.nirmal.algoExpert.search;
+
+public class BinarySearch {
+    // Solution::2
+    public static int binarySearch(int[] array, int target) {
+        // Write your code here.
+
+        int left=0;
+        int right= array.length-1;
+
+        while (left<=right){
+            int m=(left+right)/2;
+            int potentialMatch=array[m];
+            if(potentialMatch == target){
+                return m;
+            } else if(target<potentialMatch){
+                right=m-1;
+            } else {
+                left=m+1;
+            }
+        }
+        return -1;
+    }
+
+    // Solution :1
+    /*public static int binarySearch(int[] array, int target) {
+        // Write your code here.
+        return binarySearchHelper(array,target,0,array.length-1);
+    }
+
+    public static int binarySearchHelper(int[] array,int target,int left,int right){
+        if(left>right){
+            return -1;
+        }
+        int m=(left+right)/2;
+        int potentialMatch=array[m];
+        if(potentialMatch == target){
+            return m;
+        } else if(target<potentialMatch){
+           return binarySearchHelper(array,target,left,m-1);
+        } else {
+            return binarySearchHelper(array,target,m+1,right);
+        }
+    }*/
+
+    public static void main(String[] args) {
+        System.out.println(BinarySearch.binarySearch(new int[]{0, 1, 21, 33, 45, 45, 61, 71, 72, 73},33));
+    }
+}
