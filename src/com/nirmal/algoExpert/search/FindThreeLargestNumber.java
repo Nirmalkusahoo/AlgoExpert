1d58a58f46eb72f67db60b313f550bc316095118
Nirmal Kumar Sahoo <nirmal.kumar.sahoo@sap.com>
2021-12-02 10:09:07 +0530

    MinNumberOfCoinsForChange FindThreeLargestNumber

---
+++ src/com/nirmal/algoExpert/search/FindThreeLargestNumber.java
@@ -0,0 +1,38 @@
+package com.nirmal.algoExpert.search;
+
+import java.util.Arrays;
+
+public class FindThreeLargestNumber {
+    public static int[] findThreeLargestNumbers(int[] array) {
+        // Write your code here.
+        int[] largest=new int[3];
+        Arrays.fill(largest,Integer.MIN_VALUE);
+        for(int num:array){
+            if(num>largest[2]){
+                updateLargest(largest,num,2);
+            } else if(num>largest[1]){
+                updateLargest(largest,num,1);
+            } else if(num>largest[0]){
+                updateLargest(largest,num,0);
+            }
+        }
+        for(int num:largest){
+            System.out.print(num+ " ");
+        }
+        return largest;
+    }
+
+    public static void updateLargest(int[] largest,int num,int index){
+        for(int i=0;i<=index;i++){
+            if(i==index){
+                largest[i]=num;
+            } else {
+                largest[i]=largest[i+1];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FindThreeLargestNumber.findThreeLargestNumbers(new int[]{10,5,9,10,12});
+    }
+}
